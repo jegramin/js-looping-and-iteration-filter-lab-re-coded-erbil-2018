2458ace475c33a27bf57dc77e317c566f155c156
Mohammed Ameen <jegroamin@gmail.com>
2018-08-18 17:34:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(arr, value){
+  let newArr=[];
+  return arr.filter(function(str){
+      if(str.toUpperCase() === value.toUpperCase()){
+        return str;
+      }
+  })
+}
+
+function fuzzyMatch(arr,val){
+  return arr.filter(function(str){
+    if(str[0]+str[1]===val){
+      return str;
+    }
+  })
+}
+
+function matchName(){
+  
+}
